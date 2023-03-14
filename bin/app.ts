26bdea1374fc52c0214648bcdab0a28779318a78
s_kojima <s_kojima@avelio.co.jp>
2023-03-14 12:11:48 +0900

    Fix: .envファイルを読み込めるようにする

--- bin/app.ts
+++ bin/app.ts
@@ -2,11 +2,14 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SetupVpcStack } from '../lib/setup-vpc-stack';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = new cdk.App();
 const env = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEPLOY_ACCOUNT,
+  region: process.env.CDK_DEPLOY_REGION,
 };
 
 new SetupVpcStack(app, 'SetupVpcStack', { env });
